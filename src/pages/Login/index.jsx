c6fd5570f2baf89f9bd3a2e63263f7402e91fa1d
Biswanath Tewari <biswanath@geekyants.com>
2022-03-18 22:00:12 +0530

    feat: ✨ dummy auth

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,9 +2,49 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 import { Navbar, Footer } from '../../components'
+import { useAuthForm } from '../../hooks'
+import { actions, useGlobalState, useUser } from '../../store'
 import './styles.scss'
 
 const Login = () => {
+  const {
+    creds,
+    error,
+    onBlurHandler,
+    onChangeHandler,
+    validateForm,
+    resetForm,
+  } = useAuthForm('', true)
+  const { showToast } = useGlobalState()
+  const { dispatchUser } = useUser()
+
+  const onSubmitHandler = e => {
+    e.preventDefault()
+    if (validateForm()) {
+      if (creds.email === 'user@gmail.com' && creds.password === '123456') {
+        dispatchUser({
+          type: actions.login,
+          payload: { name: 'bizan', email: creds.email },
+        })
+        showToast({
+          message: 'Success!',
+          type: 'success',
+        })
+        resetForm()
+      } else {
+        showToast({
+          message: 'Oops! Wrong email or password',
+          type: 'failed',
+        })
+      }
+    } else {
+      showToast({
+        message: 'Bhai kya kar raha hai tu?',
+        type: 'failed',
+      })
+    }
+  }
+
   return (
     <div className="app">
       <Navbar />
@@ -20,10 +60,14 @@ const Login = () => {
               <input
                 id="email"
                 type="email"
-                className="form-field"
+                value={creds.email}
+                onChange={onChangeHandler}
+                onBlur={onBlurHandler}
+                className={`form-field ${error.email ? 'form-error' : ''}`}
                 placeholder="bizan@hooli.com"
                 required
               />
+              {error.email && <h1 className="error-text">{error.email}</h1>}
             </div>
             <div className="input-container">
               <label
@@ -35,10 +79,16 @@ const Login = () => {
               <input
                 id="password"
                 type="password"
-                className="form-field"
+                value={creds.password}
+                onChange={onChangeHandler}
+                onBlur={onBlurHandler}
+                className={`form-field ${error.password ? 'form-error' : ''}`}
                 placeholder="main nahi bataunga"
                 required
               />
+              {error.password && (
+                <h1 className="error-text">{error.password}</h1>
+              )}
             </div>
             <div className="conditions">
               <div className="flex-center">
@@ -46,7 +96,7 @@ const Login = () => {
                   type="checkbox"
                   id="remember"
                   className="form-checkbox"
-                  checked
+                  defaultChecked
                 />
                 <p className="text-rg">Remember me</p>
               </div>
@@ -55,7 +105,12 @@ const Login = () => {
                 Forgot password?{' '}
               </a>
             </div>
-            <div type="submit" className="btn btn-primary">
+
+            <div
+              type="submit"
+              className="btn btn-primary"
+              onClick={onSubmitHandler}
+            >
               <h1 className="text-md text-700">Login</h1>
             </div>
           </form>
--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
 
 import { Navbar, Footer } from '../../components'
+import { useAuthForm } from '../../hooks'
+import { actions, useGlobalState, useUser } from '../../store'
 import './styles.scss'
 
 const Signup = () => {
+  const {
+    creds,
+    error,
+    checkHandler,
+    onBlurHandler,
+    onChangeHandler,
+    validateForm,
+    resetForm,
+  } = useAuthForm('Please agree to the terms and conditions')
+  const { showToast } = useGlobalState()
+  const { dispatchUser } = useUser()
+
+  const onSubmitHandler = e => {
+    e.preventDefault()
+    if (validateForm()) {
+      const userDetails = {
+        name: 'Jian Yang',
+        email: creds.email,
+        password: creds.password,
+      }
+      dispatchUser({ type: actions.login, payload: userDetails })
+      showToast({
+        message: 'Success!',
+        type: 'success',
+      })
+      resetForm()
+    } else {
+      showToast({
+        message: 'Try again!',
+        type: 'failed',
+      })
+    }
+  }
+
   return (
     <div className="app">
       <Navbar />
@@ -19,10 +55,14 @@ const Signup = () => {
               <input
                 id="email"
                 type="email"
-                className="form-field"
+                value={creds.email}
+                onChange={onChangeHandler}
+                onBlur={onBlurHandler}
+                className={`form-field ${error.email ? 'form-error' : ''}`}
                 placeholder="bizan@hooli.com"
                 required
               />
+              {error.email && <h1 className="error-text">{error.email}</h1>}
             </div>
             <div className="input-container">
               <label
@@ -34,23 +74,40 @@ const Signup = () => {
               <input
                 id="password"
                 type="password"
-                className="form-field"
+                value={creds.password}
+                onChange={onChangeHandler}
+                onBlur={onBlurHandler}
+                className={`form-field ${error.password ? 'form-error' : ''}`}
                 placeholder="main nahi bataunga"
                 required
               />
+              {error.password && (
+                <h1 className="error-text">{error.password}</h1>
+              )}
             </div>
             <div className="conditions">
-              <div className="flex-center">
+              <div
+                className="flex-center cursor-pointer"
+                onClick={checkHandler}
+              >
                 <input
                   type="checkbox"
                   id="remember"
                   className="form-checkbox"
-                  checked={false}
+                  checked={creds.checkValue}
+                  onChange={checkHandler}
                 />
-                <p className="text-rg">I accept all Terms & Conditions</p>
+                <p className="text-rg">I accept all the Terms & Conditions</p>
               </div>
+              {error.checkValue && (
+                <h1 className="error-text">{error.checkValue}</h1>
+              )}
             </div>
-            <div type="submit" className="btn btn-primary">
+            <div
+              type="submit"
+              className="btn btn-primary"
+              onClick={onSubmitHandler}
+            >
               <h1 className="text-md text-700">Create New Account</h1>
             </div>
           </form>
